38b501f8725c9b78cc6320f96e2e8bae3d8210cb
Markussim <markus.simonsen@gmail.com>
2020-10-09 13:47:18 +0200

    FIxed spelling

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Thissssssssssssssssss is running in docker!'))
+app.get('/', (req, res) => res.send('This is running in docker!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
