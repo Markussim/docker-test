d183c5e8ee597b3b625c9d9c7784dc8c382cec79
Markussim <markus.simonsen@gmail.com>
2020-10-09 13:19:05 +0200

    Added docker-compose

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Helllllllllllllo World!'))
+app.get('/', (req, res) => res.send('leeeeeäääääääälllo World!'))
 app.listen(port, () => console.log(`Example app listening on port touch Dockerfile!`))
